3934fb0f9f1f97f6bd6268b327c414e207985fd9
unknown <sjxliu8@gmail.com>
2022-01-01 11:46:07 -0600

    finished but isn't displaying correctly

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 // DEPENDENCIES
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
 
 // CONFIGURATION
-require('dotenv').config()
-const PORT = process.env.PORT
-const app = express()
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
-    () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
-)
+require("dotenv").config();
+const PORT = process.env.PORT;
+const app = express();
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("connected to mongo: ", process.env.MONGO_URI);
+  }
+);
 
 // MIDDLEWARE
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }));
+
+// Languages
+const languageController = require("./controllers/language-controllers");
+app.use("/languages", languageController);
 
 // ROUTES
-app.get('/', (req, res) => {
-  res.send('Welcome to the Hello World! API')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to the Hello World! API");
+});
 
 // LISTEN
 app.listen(PORT, () => {
-  console.log('Greetings! From port: ', PORT);
-})
+  console.log("Greetings! From port: ", PORT);
+});
